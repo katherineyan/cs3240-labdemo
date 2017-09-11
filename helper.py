de1244be285514d9fb6c0ebc7bbf84954c4bcee7
Katherine Yan <katherine.yan2015@gmail.com>
2017-09-11 14:35:08 -0400

    changed print function

---
+++ helper.py
@@ -0,0 +1,12 @@
+#Katherine Yan, kjy2kh, 9/11/17, hello.py
+
+__author__ = 'kjy2kh'
+
+def greeting(msg):
+    print(msg)
+
+def main():
+    greeting('hello there')
+
+if __name__ == '__main__':
+    main()
